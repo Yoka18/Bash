f39acfbd66a500938301ae823a8febd81dddcdef
inmisin <omeroflu2@gmail.com>
2024-08-28 18:00:35 +0300

    bash

---
+++ systemInfo.sh
@@ -0,0 +1,14 @@
+
+
+echo "İşlem Sayısı:"
+ps -e | wc -l
+
+
+echo "Disk Kullanımı:"
+df -h
+
+
+echo "Aktif Kullanıcı:"
+whoami
+
+read -p ""
---
+++ welcome.sh
@@ -0,0 +1,5 @@
+read -p "Lütfen adınızı giriniz: " user_name
+
+echo "Hoşgeldin, $user_name!"
+
+read -p ""
